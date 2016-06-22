3bf80ecfc511b8d0eaa41b1aad66cc309894c876
Ian Storz <istorz@nestlabs.com>
2016-06-22 15:16:52 -0600

    Broccoli plugin version

---
+++ app/components/my-component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-serial-build/components/my-component';
--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 /* jshint node: true */
 'use strict';
 
+var Queue = require('./lib/queue');
+
 module.exports = {
-  name: 'ember-cli-serial-build'
+  name: 'ember-cli-serial-build',
+
+  setupPreprocessorRegistry: function(type, registry) {
+    this.queue = this.queue || new Queue();
+    var queue = this.queue;
+
+    registry.add('css', {
+      toTree: function(tree) {
+        var workingTree = queue.add(tree, {
+          registryType: type,
+          treeType: 'css'
+        });
+
+        return workingTree;
+      }
+    });
+
+    registry.add('js', {
+      toTree: function(tree) {
+        var workingTree = queue.add(tree, {
+          registryType: type,
+          treeType: 'js'
+        });
+
+        return workingTree;
+      }
+    });
+
+    registry.add('template', {
+      toTree: function(tree) {
+        var workingTree = queue.add(tree, {
+          registryType: type,
+          treeType: 'template'
+        });
+
+        return workingTree;
+      }
+    });
+  }
 };
